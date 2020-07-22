642c4164db6fb15f9d47b612ca5afea63cc883e2
Teknoist <37031361+Teknoist@users.noreply.github.com>
2020-07-23 02:36:19 +0300

    Update Netflix_Install.sh

--- Netflix_Install.sh
+++ Netflix_Install.sh
@@ -3,14 +3,12 @@ sudo apt-get update 2>&1 | dialog --title "Updating package database and install
 if [ ! -d /usr/share/doc/python-crypto ]; then
 	sudo apt-get install -q python-crypto
 fi
-sudo apt-get install -q -y build-essential python-pip libnss3 libnspr4
+sudo apt-get install -q -y build-essential python-pip libnss3 libnspr4 kodi kodi-inputstream-adaptive
 
 dialog --title "Installing python dependencies..." --infobox "\nPlease wait...\n" 11 70
 sudo pip install -q -U setuptools
 sudo pip install -q wheel
 sudo pip install -q pycryptodomex
-sudo apt-get install kodi
-sudo apt-get install kodi-inputstream-adaptive
 sleep 5
 echo 'pi ALL=NOPASSWD:/bin/mount,/bin/umount,/sbin/losetup,/sbin/modprobe"' >> /etc/sudoers
 sleep 5
